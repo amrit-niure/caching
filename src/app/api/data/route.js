b02157642ff550fa4bae75c40293d8338095e1f6
Amrit Niure <niureamrit2@gmail.com>
2023-09-11 12:11:40 +1000

    Initial Commit ..

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -9,6 +9,7 @@ export async function GET(req){
         if (!posts) {
             return NextResponse.json({ success: false, message: 'Post not found' }, { status: 400 });
         }
+        revalidatePath(req.url)
         return NextResponse.json({ success:true, posts:posts}, { status: 200 });
     } catch (error) {
         console.log('Route error ',error)
